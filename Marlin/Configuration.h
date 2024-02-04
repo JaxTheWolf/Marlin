868566635308086098c280ae25dc7d273d06f8d0
Roman Lubij <roman.lubij@gmail.com>
2024-02-04 16:45:19 +0100

    enable edge stepping, speed up probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1592,7 +1592,7 @@
 #define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (100*60)
+#define XY_PROBE_FEEDRATE (200*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (4*60)
@@ -1645,7 +1645,7 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 3
+#define MULTIPLE_PROBING 2
 #define EXTRA_PROBING    1
 
 /**
